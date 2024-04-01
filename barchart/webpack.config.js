e73a3c4c48550875e77dadf3547221063baba38c
EDWIN GEORGE LAZER <edwin04@gmail.com>
2024-04-01 22:16:46 +0100

    first microfrontend

---
+++ barchart/webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    mode:'development',
+    devServer:{
+        port: 1001
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html'
+        })
+    ]
+}
